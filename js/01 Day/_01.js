6eaf7fcc71a95fff6d47d8ff7d56182ab2c685a0
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-04-14 20:45:06 +0300

    Home task is done

---
+++ js/01 Day/_01.js
@@ -0,0 +1,39 @@
+"use strict";
+//! 1
+let a = 2;
+let squareP = 4 * a;
+// console.log(`Square P equals to ${squareP}`);
+
+//! 2
+let squareA = 3;
+let squareS = squareA ** 2;
+// console.log(`Square S equals to ${squareS}`);
+
+//! 3
+let SquareA = 4;
+let SquareB = 4;
+let SquareP = SquareA * SquareB;
+let SquareS = 2 * (SquareA + SquareB);
+// console.log(`Square P equals to ${SquareP} and square S equals to ${SquareS}`);
+
+//! 4
+const P = 3.14;
+let circleD = 4;
+let circleL = P * circleD;
+//console.log(`Circle L equals to ${circleL}`);
+
+//! 5
+let cubeA = 2;
+let cubeV = cubeA ** 2 * cubeA;
+let cubeS = 6 * cubeA ** 2;
+// console.log(`Cube V equals to ${cubeV} and cube S equals to ${cubeS}`);
+
+//! 6
+let parA = 5;
+let parB = 6;
+let parC = 7;
+let parV = parA * parB * parC;
+let parS = 2 * (parA * parB + parB * parC + parA * parC);
+console.log(
+  `Paralelogram V equals to ${parV} and paralelofram S equals to ${parS}`
+);
