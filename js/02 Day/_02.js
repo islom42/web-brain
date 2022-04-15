b03ac1ce553a4e630bae077383a5c83c4d3c7c07
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-04-15 11:41:09 +0300

    Finished with homework

--- js/02 Day/_02.js
+++ js/02 Day/_02.js
@@ -1,7 +1,8 @@
-var a = 2;
-var a = 5;
+//! Make random number from 100 and 200
+let getRandomNumber = Math.floor(Math.random() * (200 - 100 + 1) + 100);
+console.log(getRandomNumber);
 
-console.log(a);
-
-let a = 3;
-let a = 5;
+//! Return nearest divisible number by 5
+let randomNumber = 28;
+const DIVISBLE = 5;
+console.log(Math.round(randomNumber / DIVISBLE) * DIVISBLE);
