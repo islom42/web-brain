4804f762c66ae950d4d30d762f3f0bec5bfd961c
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-05-12 01:48:46 +0300

    Advanced functin and Date object

---
+++ js/11 Day/11.js
@@ -0,0 +1,25 @@
+// Recursive function
+//! Normal fucntion with for loop
+/* function countDonw(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
+  console.log("Webbrain Academy");
+}
+countDonw(11); */
+
+//! Recursive function
+/* function countDonwRecursive(n) {
+  if (n <= 0) {
+    console.log("Webbrain academy");
+    return;
+  }
+  console.log(n);
+  countDonwRecursive(n - 1);
+}
+countDonwRecursive(5); */
+
+//================================
+//================================
+
+//! Data Unix
---
+++ js/11 Day/README.md
@@ -0,0 +1,7 @@
+[Image](./images/date_time_object.png)
+
+[Task 1](./images/photo_2022-05-12_01-45-13.jpg)
+
+[Task 2](./images/photo_2022-05-12_01-45-28.jpg)
+
+[Task 3](./images/photo_2022-05-12_01-45-36.jpg)
